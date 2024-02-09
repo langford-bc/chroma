f5e5c9b59ca4c87ecd7c34937f5b992e481569e4
langford-bc <sk283@caa.columbia.edu>
2024-02-09 14:59:44 +0100

    adds code for chunking using recursive character text splitter

--- embedding.py
+++ embedding.py
@@ -1,5 +1,6 @@
 #from helper_utils import word_wrap # helper functions from utilities
 from pypdf import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 reader = PdfReader("/Users/deen/Documents/chromadb-test/tut-chroma-clearai/microsoft_annual_report_2022.pdf")
 pdf_texts = [p.extract_text().strip() for p in reader.pages]
@@ -7,7 +8,19 @@ pdf_texts = [p.extract_text().strip() for p in reader.pages]
 # Filter the empty strings
 pdf_texts = [text for text in pdf_texts if text]
 
-print(pdf_texts[0])
+#print(pdf_texts[0])
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter, SentenceTransformersTokenTextSplitter
+
+character_splitter = RecursiveCharacterTextSplitter(
+    separators=["\n\n", "\n", ". ", " ", ""],
+    chunk_size=1000,
+    chunk_overlap=0
+)
+character_split_texts = character_splitter.split_text('\n\n'.join(pdf_texts))
+
+print(character_split_texts[10])
+print(f"\nTotal chunks: {len(character_split_texts)}")
 
 
 
